bc651aa97e0f5a3075dfe64c22ef294b2512abf0
缘空空 <2197960230@qq.com>
2021-02-14 08:43:44 +0800

    单独关闭passwall测试

--- Scripts/AutoBuild_DiyScript.sh
+++ Scripts/AutoBuild_DiyScript.sh
@@ -14,7 +14,7 @@ Diy_Core() {
 	INCLUDE_Enable_FirewallPort_53=true
 
 	INCLUDE_SSR_Plus=true
-	INCLUDE_Passwall=true
+	INCLUDE_Passwall=false
 	INCLUDE_HelloWorld=true
 	INCLUDE_Bypass=true
 	INCLUDE_OpenClash=true
